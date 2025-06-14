2a46344225b6ba0eb3c27b280cb4274c94bf1997
engendromestre <engendro.mestre@gmail.com>
2025-06-14 14:43:25 -0300

    Projeto ControleAcademico criado e modelo Aluno testado

---
+++ ControleAcademico/src/main/App.java
@@ -0,0 +1,25 @@
+package main;
+
+import model.Aluno;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Pedro Henrique");
+        aluno.setEndereco("Convivio");
+        aluno.setIdade(17);
+        aluno.setMatricula(1);
+
+        // System.out.println(aluno);
+        aluno.visualizar();
+
+        Aluno aluno2 = new Aluno(
+            "Peterson Alves",
+            "Cohab 2",
+            30,
+            2
+        );
+        System.out.println();
+        aluno2.visualizar();
+    }
+}
