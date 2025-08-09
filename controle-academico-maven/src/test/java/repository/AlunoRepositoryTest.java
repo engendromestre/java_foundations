a86d665bd3eff3ff51ca7a9ad324a95ebff773b4
engendromestre <engendro.mestre@gmail.com>
2025-08-09 13:42:31 -0300

    Add AlunoRepositoryTest

--- controle-academico-maven/src/test/java/repository/AlunoRepositoryTest.java
+++ controle-academico-maven/src/test/java/repository/AlunoRepositoryTest.java
@@ -1,11 +1,29 @@
 package repository;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+
+import org.junit.jupiter.api.BeforeAll;
 
 public class AlunoRepositoryTest {
     private static Path tempDir;
     private static Path txtFile;
     private static Path csvFile;
 
-    
+    @BeforeAll
+    static void setupDir() throws IOException {
+        tempDir = Files.createTempDirectory("alunoRepoTest");
+        txtFile = tempDir.resolve("aluno.txt");
+        csvFile = tempDir.resolve("alunos.csv");
+
+        // Redefinir caminhos para arquivos temporários
+        try {
+            var txtField = AlunoRepository.class.getDeclaredField("ARQUIVO_TXT");
+            txtField.setAccessible(true);
+            txtField.set(null,txtFile.toString());
+        } catch(Exception e) {
+            throw new RuntimeException("Falha ao redefinir caminho de arquivo", e);
+        }
+    }
 }
